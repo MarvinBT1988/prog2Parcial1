b57d89bbfc6e00d3d15c7138342d2a8740ae8375
josue diaz <hernandzmarlon8@gmail.com>
2024-08-29 18:33:39 -0600

    Clases empleado y planilla

---
+++ Prog2Parcial1.Domain/Empleado.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog2Parcial1.Domain
+{
+	public  class Empleado
+	{
+
+		public string EmpleadoId { get; set; }
+
+		public string Nombre { get; set; }
+
+		public string Apellido { get; set; }
+
+		public string Area { get; set; }
+
+		public string Dui {  get; set; }
+
+		public decimal Salario { get; set; }
+
+        public Empleado()
+        {
+				
+        }
+
+
+        public Empleado( string nombre, string apellido, string area, string dui, decimal salario )
+        {
+			Nombre = nombre;
+			Apellido = apellido;
+			Area = area;
+			Dui = dui;
+			Salario = salario;
+        }
+    }
+}
---
+++ Prog2Parcial1.Domain/Planilla.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog2Parcial1.Domain
+{
+	internal class Planilla
+	{
+
+		public decimal Salario { get; set; }
+
+        private decimal ISSS {  get; set; }
+
+        private decimal AFP { get; set; }
+
+        private decimal Renta { get; set; }
+
+        public Planilla ( decimal salario)
+        {
+            Salario = salario;
+        }
+
+
+        public decimal CalcularISSS()
+        {
+
+            ISSS = Salario * 0.03m;
+            if (ISSS > 30)
+            {
+                ISSS = 30;
+            }
+            return ISSS;
+        }
+
+        public decimal CalcularAFP()
+        {
+            AFP = Salario * 0.0725m;
+            return AFP;
+        }
+
+        public decimal CalcularRenta() 
+        {
+            if (Renta > 472.1m & Renta < 895.24m)
+            {
+                Renta = Salario * 0.10m;
+            }
+            else if(Renta >= 895.24m &  Renta < 2038.10m)
+            {
+                Renta = Salario * 0.20m;
+            }
+            else if ( Renta >= 2038.10m )
+            {
+                Renta = Salario * 0.30m;
+            }
+
+            return Renta;
+   
+        }
+
+    }
+
+
+
+}
