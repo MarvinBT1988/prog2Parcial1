c6af008c4c46bc5c4cc1d62bdf1351479c6aca0c
josue diaz <hernandzmarlon8@gmail.com>
2024-08-29 19:01:22 -0600

    Nueva variable

--- Prog2Parcial1.Domain/Planilla.cs
+++ Prog2Parcial1.Domain/Planilla.cs
@@ -6,27 +6,35 @@ using System.Threading.Tasks;
 
 namespace Prog2Parcial1.Domain
 {
-	public class Planilla
-	{
+    public class Planilla
+    {
 
-		public decimal Salario { get; set; }
+        public decimal Salario { get; set; }
 
-        private decimal ISSS {  get; set; }
+        private decimal ISSS { get; set; }
 
         private decimal AFP { get; set; }
 
         private decimal Renta { get; set; }
 
+        private decimal SalarioMenosISSYAFP { get; set; }
+
 
         public Planilla()
         {
-            
+
         }
 
-        public Planilla ( decimal salario)
+        public Planilla(decimal salario)
         {
             Salario = salario;
         }
+
+        private decimal  ObtenerSalarioMenosISSYAFP(){
+             SalarioMenosISSYAFP = Salario - CalcularISSS() - CalcularAFP();
+            return SalarioMenosISSYAFP;
+
+		}
 
 
         public decimal CalcularISSS()
@@ -48,17 +56,19 @@ namespace Prog2Parcial1.Domain
 
         public decimal CalcularRenta() 
         {
-            if (Renta > 472.1m & Renta < 895.24m)
+            SalarioMenosISSYAFP = ObtenerSalarioMenosISSYAFP();
+
+            if (SalarioMenosISSYAFP > 472.1m & SalarioMenosISSYAFP < 895.24m)
             {
-                Renta = Salario * 0.10m;
+                Renta = SalarioMenosISSYAFP * 0.10m;
             }
-            else if(Renta >= 895.24m &  Renta < 2038.10m)
+            else if(SalarioMenosISSYAFP >= 895.24m & SalarioMenosISSYAFP < 2038.10m)
             {
-                Renta = Salario * 0.20m;
+                Renta = SalarioMenosISSYAFP * 0.20m;
             }
-            else if ( Renta >= 2038.10m )
+            else if ( SalarioMenosISSYAFP >= 2038.10m )
             {
-                Renta = Salario * 0.30m;
+                Renta = SalarioMenosISSYAFP * 0.30m;
             }
 
             return Renta;
