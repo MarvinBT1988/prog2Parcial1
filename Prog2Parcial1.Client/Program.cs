80a9adbd0cdbe3c70c603e06854c92ef55973af0
Marvin antonio Barrera Trigueros <marvin022021@outlook.es>
2024-08-29 18:32:08 -0600

    actualizacion del formulario de planilla

--- Prog2Parcial1.Client/Program.cs
+++ Prog2Parcial1.Client/Program.cs
@@ -16,7 +16,7 @@ namespace Prog2Parcial1.Client
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmEmpleado());
         }
     }
 }
